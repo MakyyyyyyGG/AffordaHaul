4df2b19de84b34e115c562fe4afa0e437479e6f6
mark <clerigom7@gmail.com>
2026-07-09 22:57:47 +0800

    refactor: update Google Tag Manager integration; streamline script loading and remove noscript fallback

--- vite-project/app/root.tsx
+++ vite-project/app/root.tsx
@@ -34,23 +34,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
           content="bT4J4kuqIRMd6Ma3JEOr6f6CtqiHHfcmFtzTGiVnMkA"
         />
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4Q3MNTZJWZ"
+        />
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TC2XMT98');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-4Q3MNTZJWZ');`,
           }}
         />
         <Meta />
         <Links />
       </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TC2XMT98"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-            title="Google Tag Manager"
-          />
-        </noscript>
         {children}
         <ScrollRestoration />
         <Scripts />
